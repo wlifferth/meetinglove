4ddf0cf78afa8f0e26f316ba9ad1eed714570ee5
Will Lifferth <wlifferth@icloud.com>
2016-08-17 17:03:35 -0400

    Adding and removing times from a meeting

--- models.py
+++ models.py
@@ -49,14 +49,13 @@ class DBConnection():
         self.db.execute("INSERT INTO meeting VALUES(%s, %s, %s, %s)", (meetingKey, meetingName, adminKey, creationTimeStamp))
         return Meeting(meetingKey, meetingName, adminKey)
 
-
     def getMeeting(self, meetingKey):
         meetingRow = self.db.execute("SELECT * FROM meeting WHERE meetingKey='{}'".format(meetingKey)).fetchone()
         return Meeting(meetingRow[0], meetingRow[1], creationTimeStamp=meetingRow[3])
 
     def addMeetingTime(self, meetingKey, adminKey, startTime):
         meetingTimeKey = generateKey(20)
-        if self.db.execute("SELECT adminKey FROM meeting WHERE meetingKey=%s", (meetingKey)).fetchone()[0] == adminKey:
+        if self.db.execute("SELECT adminKey FROM meeting WHERE meetingKey=%s", (meetingKey)).fetchone()[0] == adminKey and not self.db.execute("SELECT * FROM meetingTime WHERE startTime=%s", (startTime)).fetchall():
             self.db.execute("INSERT INTO meetingTime VALUES(%s, %s, %s)", (meetingTimeKey, meetingKey, startTime))
             return MeetingTime(meetingTimeKey, meetingKey, startTime)
         else:
@@ -72,8 +71,21 @@ class DBConnection():
         if self.db.execute("SELECT adminKey FROM meeting WHERE meetingKey=%s", (meetingKey)).fetchone()[0] == adminKey:
             print("DELETE FROM meetingTime WHERE meetingTimeKey='{}'".format(meetingTimeKey))
             self.db.execute("DELETE FROM meetingTime WHERE meetingTimeKey=%s", (meetingTimeKey))
-        else:
-            print("ERROR")
+
+    def addPerson(self, meetingKey, personName):
+        personKey = generateKey(20)
+        while self.db.execute("SELECT COUNT(1) FROM person WHERE personKey=%s", (personKey)) == 1:
+            personKey = generateKey(20)
+        self.db.execute("INSERT INTO person VALUES (%s, %s, %s)", (personKey, meetingKey, personName))
+
+    def getPersons(self, meetingKey):
+        # Get all the rows of person that have the correct meetingKey
+        persons = self.db.execute("SELECT * FROM person WHERE meetingKey=%s", (meetingKey)).fetchall()
+        # Returns a list of Person objects
+        return [Person(person[0], person[1], person[2]) for person in persons]
+
+    def getAvails(self, meetingKey, personKey):
+        self.db.execute("SELECT * FROM person WHERE meetingKey=%s RIGHT JOIN avail ON person.personKey=avail.personKey RIGHT JOIN  meetingTime ON avail.meetingTimeKey=meetingTime.meetingTimeKey", (meetingKey))
 
 
 class Meeting():
@@ -88,3 +100,9 @@ class MeetingTime():
         self.meetingTimeKey = meetingTimeKey
         self.meetingKey = meetingKey
         self.startTime = startTime
+
+class Person():
+    def __init__(self, personKey, meetingKey, personName):
+        self.personKey = personKey
+        self.meetingKey = meetingKey
+        self.personName = personName
--- static/js/homeInteraction.js
+++ static/js/homeInteraction.js
@@ -2,3 +2,27 @@ function updateNewMeeting(name)
 {
     document.getElementById('new-meeting').action = "/newMeeting/" + encodeURIComponent(name);
 }
+
+function copy(sender)
+{
+    var target = sender.target;
+    var copytarget = target.copytarget;
+    var inp = (c ? document.querySelector(c) : null);
+
+    // Is the element selectable?
+    if(inp && inp.select)
+    {
+        // Select text
+        inp.select();
+        try
+        {
+            // Copy text
+            document.execComand('copy');
+            inp.blur();
+        }
+        catch
+        {
+            alrt('Please press Ctrl/Cmd + C to copy');
+        }
+    }	
+}
